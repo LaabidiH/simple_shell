a614f31a86963a83866c65a2c225c4c57305d770
chourak fatima ezzahra <125797129+FtimaEzzahraChourak@users.noreply.github.com>
2023-05-24 17:38:47 +0100

    Create Simple_shell_0.1.c

---
+++ Simple_shell_0.1.c
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Insufficient arguments.\n");
+        return 1;
+    }
+
+    int num1 = std::atoi(argv[1]);
+    int num2 = std::atoi(argv[2]);
+
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", difference);
+
+    return 0;
+}
