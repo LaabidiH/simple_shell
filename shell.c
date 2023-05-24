ebc971c168a50ac7c7488e3c98383ae49959decb
Houda LAABIDI <houd.laabidi@gmail.com>
2023-05-24 09:34:43 -0700

    tasks

--- shell.c
+++ shell.c
@@ -1,19 +1,18 @@
 #include "shell.h"
 /**
- * shell - Writing my shell.
- * @prompt : to write the text
- * @lineptr : the line pointer
- * @n : the size
+ * shell - Writing my shell
+ * @ac : declaring void variable
+ * @argv : declaring void variable
  *
  * Return: Always 0 (Success)
  */
-int shell(int ac, char **argv){
+int shell(int ac, char **argv)
+{
 char *prompt = "(myShell) $ ";
 char *lineptr;
-size_t n = 0; 
-
-  /* declaring void variables */
-(void)ac; (void)argv;
+size_t n = 0;
+(void)ac;
+(void)argv;
 printf("%s", prompt);
 getline(&lineptr, &n, stdin);
 printf("%s\n", lineptr);
