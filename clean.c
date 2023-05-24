64429c73fc19aa9f49c4c8caaecf446f58b68a0f
Houda LAABIDI <houd.laabidi@gmail.com>
2023-05-24 08:20:16 -0700

    tasks

---
+++ clean.c
@@ -0,0 +1,11 @@
+#include <shell.h>
+/**
+ * main - Writing a beautiful program that passes the Betty checks, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int clean()
+{
+printf("hello world\n");
+return (1);
+}
