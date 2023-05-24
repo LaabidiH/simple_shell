501865817196a77017fcca38de07d6ffca923092
Houda LAABIDI <houd.laabidi@gmail.com>
2023-05-24 08:02:42 -0700

    tasks

---
+++ beautiful_code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Writing a beautiful program that passes the Betty checks.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("hello world\n");
+return (1);
+}
