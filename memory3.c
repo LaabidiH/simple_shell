c1eef2e93c332c8442884da7ff7fb0ca89374c43
chourak fatima ezzahra <125797129+FtimaEzzahraChourak@users.noreply.github.com>
2023-05-26 22:07:47 +0100

    Update memory3.c

--- memory3.c
+++ memory3.c
@@ -1,65 +1,65 @@
 #include "shell.h"
 
 /**
- * active - returns true if shell is interactive mode
+ * interactive - returns true if shell is interactive mode
  * @info: struct address
  *
- * Return: 1 if active mode, 0 otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int active(comm_t *info)
+int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * delim - checks if character is a delimeter
+ * is_delim - checks if character is a delimeter
  * @c: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int delim(char a, char *c_delim)
+int is_delim(char c, char *delim)
 {
-	while (*c_delim)
-		if (*delim++ == a)
+	while (*delim)
+		if (*delim++ == c)
 			return (1);
 	return (0);
 }
 
 /**
- * alpha - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: The character to input
  * Return: 1 if c is alphabetic, 0 otherwise
  */
 
-int alpha(int a)
+int _isalpha(int c)
 {
-	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
 /**
- * atoi - converts a string to an integer
+ * _atoi - converts a string to an integer
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
 
-int atoi(char *w)
+int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; w[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		if (w[i] == '-')
+		if (s[i] == '-')
 			sign *= -1;
 
-		if (w[i] >= '0' && w[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (w[i] - '0');
+			result += (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
