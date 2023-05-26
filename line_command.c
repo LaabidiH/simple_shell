0eb6624ac0f6d28d4c6fa21c5553fc9ea1bc1682
chourak fatima ezzahra <125797129+FtimaEzzahraChourak@users.noreply.github.com>
2023-05-26 17:58:50 +0100

    Update line_command.c

--- line_command.c
+++ line_command.c
@@ -2,6 +2,13 @@
 #include <cstdio>
 #include <cstring>
 #include <unistd.h>
+/**
+ * myExit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
+ */
 
 int myExit(info_t *info)
 {
